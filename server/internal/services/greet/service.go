926bb4c1da33089839cc770610fdc503556a1557
Golden Kumar <auenkumar64@gmail.com>
2026-08-19 10:52:20 +0530

    wip: divided implementation of service and service.go file

--- server/internal/services/greet/service.go
+++ server/internal/services/greet/service.go
@@ -2,30 +2,14 @@
 package greet
 
 import (
-	"context"
-	"fmt"
-
 	"connectrpc.com/connect"
 	"connectrpc.com/validate"
-	greetv1 "github.com/auenkr/go-templates/server/gen/proto/greet/v1"
 	"github.com/auenkr/go-templates/server/gen/proto/greet/v1/greetv1connect"
 	"github.com/auenkr/go-templates/server/internal/interceptors"
 	"github.com/auenkr/go-templates/server/pkg/server"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
-
-type Service struct{}
-
-var _ greetv1connect.GreetServiceHandler = (*Service)(nil)
-
-func (s *Service) Greet(ctx context.Context, req *connect.Request[greetv1.GreetRequest]) (*connect.Response[greetv1.GreetResponse], error) {
-	return &connect.Response[greetv1.GreetResponse]{
-		Msg: &greetv1.GreetResponse{
-			Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
-		},
-	}, nil
-}
 
 type ServiceParams struct {
 	fx.In
